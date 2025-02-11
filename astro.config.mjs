7872fa100a8b1108d004d4173ba8f0b3024feea6
Ricardo Vega G. <rikytoloser@gmail.com>
2025-02-10 19:42:08 -0600

    add rect for use in the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
+import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -10,4 +11,5 @@ export default defineConfig({
   site: "https://link.itssofi.dev",
   adapter: vercel(),
   output: "server",
+  integrations: [react()],
 });
