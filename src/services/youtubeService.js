99cdc0482ddb1f07e2a0343a617c012ba0369e8d
SofiDev <angiemeow89@gmail.com>
2025-03-01 00:25:08 -0600

    🐛 Bugs

--- src/services/youtubeService.js
+++ src/services/youtubeService.js
@@ -1,16 +1,3 @@
-
-import YoutubeData from '@data/youtubeChannels.json';
-
-
-const channelId = YoutubeData.map(data => data.split("channel/")[1])
- /*
-    Acá está devolviendo dos channelId
-
-    "UC36_js-krsAHAEAWpEDhHtw",
-  "UClYZjPD_hW5kPkzlzWyMLrw"
- */
-console.log(channelId);
-
 export const fetchYouTubeVideos = async (channelId) => {
     const channelURL = encodeURIComponent(`https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`);
     const reqURL = `https://api.rss2json.com/v1/api.json?rss_url=${channelURL}`;
